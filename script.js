67a8410f2e1c604ec37f638f03369d57238951ca
sol.obiny@gmail.com <sol.obiny@gmail.com>
2026-05-21 23:34:40 +0300

    implemented menu item activation based on scroll position

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const navbar = document.querySelector("#navbar");
+const menuItems = document.querySelectorAll(".menu_item");
+const sections = document.querySelectorAll("section");
 
 window.addEventListener("scroll", () => {
   if (scrollY > 0) {
@@ -15,3 +17,32 @@ navbar.addEventListener("click", (e) => {
   const clicked = e.target.closest(".menu_item");
   clicked.classList.add("focused");
 });
+
+const observerOptions = {
+  root: null,
+  rootMargin: "-20% 0px -70% 0px", // tweak these values
+  threshold: 0,
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // Remove active from all links
+      menuItems.forEach((link) => link.classList.remove("focused"));
+
+      // Find corresponding nav link and activate it
+      let activeLink = document.querySelector(
+        `.menu_item[href="#${entry.target.id}"]`,
+      );
+
+      // console.log(entry.target.id === "hero");
+      if (entry.target.id === "hero") {
+        activeLink = document.querySelector('.menu_item[href="#"]');
+      }
+      if (activeLink) activeLink.classList.add("focused");
+    }
+  });
+}, observerOptions);
+
+// 2. Observe all sections
+sections.forEach((section) => observer.observe(section));
