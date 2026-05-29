3034cfe2ce2e85be7a90b7e69ceccc95d99090a0
sol.obiny@gmail.com <sol.obiny@gmail.com>
2026-05-29 23:54:59 +0300

    slider + legibility changes in navbar and hero text

--- script.js
+++ script.js
@@ -12,6 +12,95 @@ const tappableElements = [
   ".footer_social_icons",
 ];
 
+// Slides MUST be more than two to work
+// Visibility MUST NOT be one of the transitioned properties transition-property CANNOT BE "all";
+const SLIDER_TIMING = 8000;
+const slides = document.querySelectorAll(".slider_img");
+
+let slidesArray = [];
+let transition;
+
+function createSlidesObjectArray(slidesNodeList) {
+  slidesNodeList.forEach((slide, i, arr) =>
+    // This is the brain of the operation: creates the logic that ensures the slide in the viewport is the second to last slide in the reel when the DOM is first loaded. Does this by giving a position property to all slides 0 is always the slide in the viewport. At position 1 is the slide next to it to the right. On the left, starting from the nearest to it is position -1, -2, -3, etc. In that order
+    slidesArray.push({
+      slide: slide,
+      position: i <= 1 ? i : i - arr.length,
+      transitions: {},
+    }),
+  );
+
+  // This one is an improvement from last implementation
+  function getTransitionProperties(el) {
+    let transitionStylesObj = {};
+    Object.keys(window.getComputedStyle(el))
+      .filter((key) => key.startsWith("transition"))
+      .forEach((transitionStyle) => {
+        transitionStylesObj[transitionStyle] =
+          window.getComputedStyle(el)[transitionStyle];
+      });
+    return transitionStylesObj;
+  }
+
+  slidesArray.forEach((slide, i, slides) => {
+    // Storing the transition property of each slide... maybe overkill but thorough
+    slide.transitions = getTransitionProperties(slide.slide);
+    // Removing the transition property to hide the transform at initialization
+    slide.slide.style.transition = "none";
+
+    // Simply arranges the slides based on their position property
+    slide.slide.style.transform = `translateX(${slide.position * 100}%)`;
+  });
+}
+
+function arrangeSlides() {
+  slidesArray.forEach((slide, i, slides) => {
+    // Returning the transition property
+    // slide.slide.style.transition = transition;
+    Object.keys(slide.transitions).forEach((transitionStyle) => {
+      slide.slide.style[transitionStyle] = slide.transitions[transitionStyle];
+    });
+  });
+
+  slidesArray.forEach((slide, i, slides) => {
+    // Creating the logic that ensures the slide about to move has opacity 0 so that the movement is not visible
+    if (
+      (slide.lastPosChange > 0 &&
+        slide.position - slide.lastPosChange === 1 - slides.length) ||
+      (slide.lastPosChange < 0 && slide.position - slide.lastPosChange === 2)
+    ) {
+      // slide.slide.style.opacity = "0";
+      slide.slide.style.visibility = "hidden";
+    } else {
+      // slide.slide.style.opacity = "1";
+      slide.slide.style.visibility = "visible";
+    }
+    slide.slide.style.transform = `translateX(${slide.position * 100}%)`;
+  });
+}
+
+function moveSlides(count) {
+  slidesArray.forEach((slide, i, slides) => {
+    // Storing the change count so that it can be used in the next step of arranging slides
+    slide.lastPosChange = count;
+    // This is the heart of the operation: creates the logic that ensures the slide in the viewport remains the second to last frame in the reel when the slides are shifted
+    if (slide.position + count > 1) slide.position = 2 - slides.length;
+    else if (slide.position + count < 2 - slides.length) slide.position = 1;
+    else {
+      slide.position += count;
+    }
+  });
+}
+
+createSlidesObjectArray(slides);
+moveSlides(1);
+arrangeSlides();
+
+setInterval(() => {
+  moveSlides(1);
+  arrangeSlides();
+}, SLIDER_TIMING);
+
 window.addEventListener("scroll", () => {
   if (menuHamburger.checked) return;
   if (scrollY > 0) {
@@ -20,8 +109,6 @@ window.addEventListener("scroll", () => {
     navbar.classList.remove("navbar-scroll");
   }
 });
-
-// console.log(document.querySelectorAll(tappableElements));
 
 document.querySelectorAll(tappableElements).forEach((item) => {
   item.addEventListener("touchstart", () => item.classList.add("tap"), {
